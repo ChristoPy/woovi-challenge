c0f0870c78cbbebaeabd6a5efcb246b2bb024387
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-07 14:35:38 -0300

    Enhance producs load.
    Make the compiler happy about the products path.
    Add a nice loading indicator on top of everything while the query runs.

--- store/src/components/products-section/index.tsx
+++ store/src/components/products-section/index.tsx
@@ -1,10 +1,22 @@
-import { graphql } from "relay-runtime"
+import { OperationType, graphql } from "relay-runtime"
 import ProductCard from "../product-card"
 import { useLazyLoadQuery } from "react-relay"
 import { Product } from "../product-card/index"
+import { useEffect, useState } from "react"
+import { useTransition, animated } from 'react-spring'
+import Loading from "../loading"
+
+interface ProductsSectionQuery extends OperationType {
+  readonly response: ProductsSectionQueryResult;
+}
+
+interface ProductsSectionQueryResult {
+  products: Product[]
+}
+
 
 export default function ProductsSection() {
-  const data = useLazyLoadQuery(
+  const { products } = useLazyLoadQuery<ProductsSectionQuery>(
     graphql`
       query productsSectionQuery {
         products {
@@ -23,14 +35,33 @@ export default function ProductsSection() {
     {}
   )
 
+  const [showSpinner, setShowSpinner] = useState<boolean>(true);
+  const transition = useTransition(showSpinner, {
+    from: { opacity: 1 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  useEffect(() => {
+    if (products) {
+      setShowSpinner(false);
+    }
+  }, [products]);
+
   return (
     <section className="py-8">
       <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-        {
-          data?.products.map((product: Product) => (
-            <ProductCard product={product} key={product._id} />
-          ))
-        }
+      {transition((style, item) => 
+          item ? (
+            <animated.div style={style}>
+              <Loading />
+            </animated.div>
+          ) : (
+            products?.map((product: Product) => (
+              <ProductCard product={product} key={product._id} />
+            ))
+          )
+        )}
       </div>
     </section>
   )
