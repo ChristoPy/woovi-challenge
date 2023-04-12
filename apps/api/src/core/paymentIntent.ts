75f36a69e59795bd2b0a0054856ea0cbd0dcc2eb
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-12 13:57:23 -0300

    Update how installments are created.
    Load variables from environment, so we can adjust as we want.
    Update the installment fee algorithm to something more real world.

--- apps/api/src/core/paymentIntent.ts
+++ apps/api/src/core/paymentIntent.ts
@@ -1,7 +1,11 @@
 import { FullInstallment, Installment, PaymentIntent } from "data";
 
+const MAX_INSTALLMENTS = Number(process.env.MAX_INSTALLMENTS);
+const FEE_PER_INSTALLMENT = Number(process.env.FEE_PER_INSTALLMENT);
+const CASHBACK_PERCENTAGE = Number(process.env.CASHBACK_PERCENTAGE)
+
 function makeFullInstallment(total: number): FullInstallment {
-  const cashbackValue = total * 0.03;
+  const cashbackValue = total * CASHBACK_PERCENTAGE;
 
   return {
     count: 1,
@@ -17,9 +21,9 @@ function makeInstallment(count: number, fee: number, total: number): Installment
 }
 
 function makeInstallments(total: number): Installment[] {
-  return Array.from({ length: 9 }, (_, i) => i + 2).map(count => {
-    const fee = count * 2;
-    const installmentTotal = total * (1 + fee);
+  return Array.from({ length: MAX_INSTALLMENTS - 1 }, (_, i) => i + 2).map(count => {
+    const fee = count * FEE_PER_INSTALLMENT;
+    const installmentTotal = total + fee;
     return makeInstallment(count, fee, installmentTotal);
   });
 }
