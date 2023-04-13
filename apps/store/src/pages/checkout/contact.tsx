5b06c68b0268b51b8268e33c462d4efb4916abf1
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-13 14:22:44 -0300

    Add dummy checkout flow.
    Better separate contact and delivery forms.

---
+++ apps/store/src/pages/checkout/contact.tsx
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import Form from "../../components/form";
+import { useState } from "react";
+
+export default function Contact() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    taxID: ""
+  });
+
+  return (
+    <section className="py-8 mt-12 px-4">
+      <div className="container mx-auto">
+        <h1 className="text-black text-2xl my-4 mb-4">Finalizar Compra</h1>
+        <div className="max-w-lg mx-auto">
+          <Form
+            title="Informações de Contato"
+            onChange={({ key, value }) => setFormData({ ...formData, [key]: value })}
+            onSubmit={() => {
+              navigate("/pagamento/endereco")
+            }}
+            inputs={[
+              { label: "Nome", id: "name" },
+              { label: "Email", id: "email", type: "email" },
+              { label: "Telefone", id: "phone", type: "phone" },
+              { label: "CPF", id: "taxID" }
+            ]}
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
--- apps/store/src/router.tsx
+++ apps/store/src/router.tsx
@@ -8,14 +8,16 @@ import Home from "./pages/home";
 import NotFound from "./pages/not-found";
 import ShoppingCart from "./pages/shopping-cart";
 import Product from "./pages/product";
-import Checkout from "./pages/checkout";
+import Contact from "./pages/checkout/contact";
+import Address from "./pages/checkout/address";
 
 export const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} >
       <Route index element={<Home />} />
       <Route path="/carrinho" element={<ShoppingCart />} />
-      <Route path="/pagamento" element={<Checkout />} />
+      <Route path="/pagamento" element={<Contact />} />
+      <Route path="/pagamento/endereco" element={<Address />} />
       <Route path="/produto/:id" element={<Product />} />
       <Route path="*" element={<NotFound />} />
     </Route>
